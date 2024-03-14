31d6d1e8ddbf15d4bb43dada11452afd8c2c3090
ebruaeo <ebruaeo@hotmail.com>
2024-03-15 01:50:04 +0300

    Ürün geçiş kodlaması yapıldı

--- app/src/main/java/com/example/foodie/fragments/ProductDetailFragment.kt
+++ app/src/main/java/com/example/foodie/fragments/ProductDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.foodie.R
 import com.example.foodie.databinding.FragmentProductDetailBinding
 
@@ -16,6 +17,14 @@ class ProductDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProductDetailBinding.inflate(inflater, container, false)
+        val bundle: ProductDetailFragmentArgs by navArgs()
+        val gelenProduct = bundle.product
+
+       binding.productName.setText(gelenProduct.product_name)
+       binding.productDescription.setText(gelenProduct.product_description)
+
+
+
         return binding.root
     }
 
