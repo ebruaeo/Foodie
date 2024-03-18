36e80015bb289d6a129154dd55b0eca6b2a3f891
ebruaeo <ebruaeo@hotmail.com>
2024-03-19 00:19:23 +0300

    view model implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.5.1")
+    implementation("androidx.activity:activity-ktx:1.6.1")
 }
