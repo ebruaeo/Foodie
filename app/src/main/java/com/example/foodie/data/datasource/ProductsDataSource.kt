f759b2500670a378af133c83c651bbf461160cda
ebruaeo <ebruaeo@hotmail.com>
2024-03-20 01:17:50 +0300

    Fonksiyon tanımlamaları

--- app/src/main/java/com/example/foodie/data/datasource/ProductsDataSource.kt
+++ app/src/main/java/com/example/foodie/data/datasource/ProductsDataSource.kt
@@ -1,10 +1,38 @@
 package com.example.foodie.data.datasource
 
 import android.util.Log
+import com.example.foodie.data.CartData
+import com.example.foodie.data.entity.Product
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class ProductsDataSource {
 
     suspend fun sil(product_id: Int) {
         Log.e("Ürün sil", product_id.toString())
     }
+
+    suspend fun productYukle(): List<Product> =
+        withContext(Dispatchers.IO) {
+
+            val list = listOf(
+                Product(0, "", 50, "Ayran", "200ml"),
+                Product(1, "", 100, "Baklava", "500gr"),
+                Product(2, "", 150, "Köfte", "1 kilo")
+            )
+
+            return@withContext list
+
+        }
+
+    suspend fun ara(aramaKelimesi: String): List<Product> =
+        withContext(Dispatchers.IO) {
+
+            val list = listOf(
+                Product(0, "", 50, "Ayran", "200ml")
+            )
+
+            return@withContext list
+
+        }
 }
--- app/src/main/java/com/example/foodie/data/repository/ProductsRepository.kt
+++ app/src/main/java/com/example/foodie/data/repository/ProductsRepository.kt
@@ -1,9 +1,13 @@
 package com.example.foodie.data.repository
 
 import com.example.foodie.data.datasource.ProductsDataSource
+import com.example.foodie.data.entity.Product
 
-class ProductsRepository {
+class ProductsRepository(var productDS:ProductsDataSource) {
 
-    var productDS = ProductsDataSource()
-    suspend fun sil(product_id: Int) = productDS.sil(product_id )
+    suspend fun sil(product_id: Int) = productDS.sil(product_id)
+
+    suspend fun productYukle(): List<Product> = productDS.productYukle()
+
+    suspend fun ara(aramaKelimesi: String): List<Product> = productDS.ara(aramaKelimesi)
 }
