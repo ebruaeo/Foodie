430d0e7a3e10b342f0a9d6ad65565570282a39d9
ebruaeo <ebruaeo@hotmail.com>
2024-03-20 01:21:28 +0300

    Silme ve yükleme fonksiyonu

--- app/src/main/java/com/example/foodie/ui/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/example/foodie/ui/viewmodel/CartViewModel.kt
@@ -1,10 +1,34 @@
 package com.example.foodie.ui.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.foodie.data.entity.Product
+import com.example.foodie.data.repository.ProductsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CartViewModel: ViewModel() {
+@HiltViewModel
+
+class CartViewModel @Inject constructor(var productRepo : ProductsRepository): ViewModel() {
+
+    var productList = MutableLiveData<List<Product>>()
+
 
     fun sil(product_id: Int) {
+        CoroutineScope(Dispatchers.Main).launch {
+            productRepo.sil(product_id)
+            productYukle()
 
+        }
     }
+
+    fun productYukle() {
+        CoroutineScope(Dispatchers.Main).launch {
+            productList.value = productRepo.productYukle()
+        }
+    }
+
 }
