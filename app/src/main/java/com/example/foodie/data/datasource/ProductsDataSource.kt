9edce63d7bcf7a158e995e6a88da0e9e2fb315cd
ebruaeo <ebruaeo@hotmail.com>
2024-03-23 02:15:02 +0300

    Arama filtreleme yapıldı.

--- app/src/main/java/com/example/foodie/data/datasource/ProductsDataSource.kt
+++ app/src/main/java/com/example/foodie/data/datasource/ProductsDataSource.kt
@@ -1,21 +1,22 @@
 package com.example.foodie.data.datasource
 
 import android.util.Log
-import com.example.foodie.data.CartData
 import com.example.foodie.data.entity.Product
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class ProductsDataSource {
+    var list = listOf<Product>()
 
     suspend fun sil(product_id: Int) {
         Log.e("Ürün sil", product_id.toString())
     }
 
+
     suspend fun productYukle(): List<Product> =
         withContext(Dispatchers.IO) {
 
-            val list = listOf(
+            list = listOf(
                 Product(0, "", 50, "Ayran", "200ml"),
                 Product(1, "", 100, "Baklava", "500gr"),
                 Product(2, "", 150, "Köfte", "1 kilo")
@@ -25,14 +26,18 @@ class ProductsDataSource {
 
         }
 
-    suspend fun ara(aramaKelimesi: String): List<Product> =
+
+    suspend fun ara(aramaKelimesi: String): ArrayList<Product> =
         withContext(Dispatchers.IO) {
+            var filteredList = arrayListOf<Product>()
+            for (i in 0..list.size - 1) {
+                if (list[i].product_name.contains(aramaKelimesi, true)) {
+                    filteredList.add(list[i])
 
-            val list = listOf(
-                Product(0, "", 50, "Ayran", "200ml")
-            )
+                }
+            }
 
-            return@withContext list
+            return@withContext filteredList
 
         }
 }
