559de7ff2b71d13bc55942a7dbb46fa3d2240ae1
ebruaeo <ebruaeo@hotmail.com>
2024-03-25 02:18:47 +0300

    CartListAdapter remove item on click

--- app/src/main/java/com/example/foodie/ui/adapter/CartListAdapter.kt
+++ app/src/main/java/com/example/foodie/ui/adapter/CartListAdapter.kt
@@ -3,21 +3,25 @@ package com.example.foodie.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.foodie.data.CartData
 import com.example.foodie.data.entity.Product
 import com.example.foodie.databinding.DesigningCartProductsBinding
 import com.example.foodie.ui.viewmodel.CartViewModel
 import com.example.foodie.ui.viewmodel.HomePageViewModel
 import com.google.android.material.snackbar.Snackbar
 
-class CartListAdapter(var cartProductList: List<Product>,
-                      var viewModel: CartViewModel) :
+class CartListAdapter(
+    var cartProductList: List<Product>,
+    var viewModel: CartViewModel
+) :
     RecyclerView.Adapter<CartListAdapter.DesigningCartHolder>() {
 
     inner class DesigningCartHolder(var designing: DesigningCartProductsBinding) :
         RecyclerView.ViewHolder(designing.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DesigningCartHolder {
-       val binding = DesigningCartProductsBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding =
+            DesigningCartProductsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return DesigningCartHolder(binding)
     }
 
@@ -26,22 +30,21 @@ class CartListAdapter(var cartProductList: List<Product>,
     }
 
     override fun onBindViewHolder(holder: DesigningCartHolder, position: Int) {
-       val product = cartProductList[position]
+        val product = cartProductList[position]
         val t = holder.designing
         t.cartProductName.text = product.product_name
-        t.cartProductCount.text =product.product_count.toString()
+        t.cartProductCount.text = product.product_count.toString()
         t.totalPrice.text = "${product.product_count * product.product_price}₺"
 
 
         t.btnDelete.setOnClickListener {
-            Snackbar.make(it,"Ürün sepetten kaldırıldı.", Snackbar.LENGTH_SHORT)
-                .setAction(""){
-                    viewModel.sil(product.product_id)
-                }.show()
+            CartData.removeProduct(product)
+            this.notifyItemRemoved(position)
+            Snackbar.make(it, "Ürün sepetten kaldırıldı.", Snackbar.LENGTH_SHORT)
+                .show()
         }
 
     }
-
 
 
 }
