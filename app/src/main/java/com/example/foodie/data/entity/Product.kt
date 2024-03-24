f7eb159a9f5fab30fac16872af8bd518364a97be
ebruaeo <ebruaeo@hotmail.com>
2024-03-25 01:09:18 +0300

    Serialized names for Product

--- app/src/main/java/com/example/foodie/data/entity/Product.kt
+++ app/src/main/java/com/example/foodie/data/entity/Product.kt
@@ -1,14 +1,21 @@
 package com.example.foodie.data.entity
 
+import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class Product(
+    @SerializedName("yemek_id")
     var product_id: Int,
+
+    @SerializedName("yemek_resim_adi")
     var product_pic: String,
+
+    @SerializedName("yemek_fiyat")
     var product_price: Int,
+
+    @SerializedName("yemek_adi")
     var product_name: String,
-    var product_description: String,
-    var product_count: Int=0
-) :
-    Serializable {
-}
+
+    var product_count: Int = 0,
+    var isFavorited: Boolean = false
+) : Serializable
