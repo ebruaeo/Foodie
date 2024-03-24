6c1f2fa8a70a26398143c13279d815503c510d51
ebruaeo <ebruaeo@hotmail.com>
2024-03-25 02:08:58 +0300

    CartData getTotalPrice, addProduct, getProductList, getProduct, isProductAlreadyAdded functions

--- app/src/main/java/com/example/foodie/data/CartData.kt
+++ app/src/main/java/com/example/foodie/data/CartData.kt
@@ -3,9 +3,42 @@ package com.example.foodie.data
 import com.example.foodie.data.entity.Product
 
 object CartData {
-    val productList = mutableListOf<Product>(
-        Product(0, "", 50, "Ayran", "200ml"),
-        Product(1, "", 100, "Baklava", "500gr"),
-        Product(2, "", 150, "Köfte", "1 kilo")
-    )
+    private val productList = mutableListOf<Product>()
+
+    fun getTotalPrice(): Int {
+        var totalPrice = 0
+        for (p in productList) {
+            totalPrice += p.product_price * p.product_count
+        }
+        return totalPrice
+    }
+
+    fun addProduct(product: Product) {
+        if (isProductAlreadyAdded(product)) {
+            val addedProduct = getProduct(product.product_id)!!
+            addedProduct.product_count += product.product_count
+        } else {
+            productList.add(product)
+        }
+    }
+
+    fun getProductList() = productList as List<Product>
+
+    private fun getProduct(productId: Int): Product? {
+        for (p in productList) {
+            if (p.product_id == productId)
+                return p
+        }
+        return null
+    }
+
+    private fun isProductAlreadyAdded(product: Product): Boolean {
+        for (p in productList) {
+            if (p.product_id == product.product_id)
+                return true
+        }
+        return false
+    }
+
+
 }
