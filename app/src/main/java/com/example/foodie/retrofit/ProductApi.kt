a006c72e4e7c6c5a3da93ab46f9742ced2053d69
ebruaeo <ebruaeo@hotmail.com>
2024-03-25 09:09:07 +0300

    ProductApi getCartProducts, removeProductFromCart, addProductToCart

--- app/src/main/java/com/example/foodie/retrofit/ProductApi.kt
+++ app/src/main/java/com/example/foodie/retrofit/ProductApi.kt
@@ -1,10 +1,37 @@
 package com.example.foodie.retrofit
 
+import com.example.foodie.data.entity.AddProductResponse
 import com.example.foodie.data.entity.AllProductsResponse
+import com.example.foodie.data.entity.CartProductsResponse
+import com.example.foodie.data.entity.RemoveProductResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ProductApi {
 
     @GET("yemekler/tumYemekleriGetir.php")
     suspend fun getAllProducts(): AllProductsResponse
+
+    @GET("yemekler/sepettekiYemekleriGetir.php")
+    suspend fun getCartProducts(): CartProductsResponse
+
+    @POST("yemekler/sepettenYemekSil.php")
+    @FormUrlEncoded
+    suspend fun removeProductFromCart(
+        @Field("sepet_yemek_id") productId: Int,
+        @Field("kullanici_adi") username: String = ApiUtils.USERNAME
+    ): RemoveProductResponse
+
+    @POST("yemekler/sepeteYemekEkle.php")
+    @FormUrlEncoded
+    suspend fun addProductToCart(
+        @Field("yemek_adi") productName: String,
+        @Field("yemek_resim_adi") productImgName: String,
+        @Field("yemek_fiyat") productPrice: Int,
+        @Field("yemek_siparis_adet") productCount: Int,
+        @Field("kullanici_adi") username: String = ApiUtils.USERNAME
+    ): AddProductResponse
+
 }
