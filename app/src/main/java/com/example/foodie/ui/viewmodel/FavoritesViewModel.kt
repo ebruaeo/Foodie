1390b21e66f1984ca8f365cd56f28bf74c9ed8bb
ebruaeo <ebruaeo@hotmail.com>
2024-03-25 15:07:26 +0300

    FavoritesViewModel fetchAllProducts + addToCart

--- app/src/main/java/com/example/foodie/ui/viewmodel/FavoritesViewModel.kt
+++ app/src/main/java/com/example/foodie/ui/viewmodel/FavoritesViewModel.kt
@@ -1,8 +1,28 @@
 package com.example.foodie.ui.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.foodie.data.CartData
+import com.example.foodie.data.entity.Product
+import com.example.foodie.data.repository.ProductsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@HiltViewModel
+class FavoritesViewModel @Inject constructor(private var productRepo: ProductsRepository) : ViewModel() {
+    var productList = MutableLiveData<List<Product>>()
+    var favoriteProductIds = setOf<String>()
 
+    fun fetchAllProducts() {
+        CoroutineScope(Dispatchers.Main).launch {
+            productList.value = productRepo.getAllProducts()
+        }
+    }
 
-class FavoritesViewModel: ViewModel() {
+    fun addToCart(product: Product) {
+        CartData.addProduct(product, 1, productRepo)
+    }
 }
