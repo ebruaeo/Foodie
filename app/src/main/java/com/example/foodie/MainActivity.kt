51fb57a824c0cb63780b44adb58fcfcecd881590
ebruaeo <ebruaeo@hotmail.com>
2024-03-25 11:20:05 +0300

    Fetch cart products on app launched

--- app/src/main/java/com/example/foodie/MainActivity.kt
+++ app/src/main/java/com/example/foodie/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.foodie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import com.example.foodie.data.CartData
+import com.example.foodie.data.repository.ProductsRepository
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var repository: ProductsRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        CartData.fetchCartProducts(repository)
     }
 }
