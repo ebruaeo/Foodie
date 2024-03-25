9ae7a5618dc41e74fad3a570f13a358b66edbcea
ebruaeo <ebruaeo@hotmail.com>
2024-03-25 08:52:41 +0300

    ApiUtils USERNAME and constructImgUrl

--- app/src/main/java/com/example/foodie/retrofit/ApiUtils.kt
+++ app/src/main/java/com/example/foodie/retrofit/ApiUtils.kt
@@ -2,10 +2,14 @@ package com.example.foodie.retrofit
 
 class ApiUtils {
     companion object {
-        val BASE_URL = "http://kasimadalan.pe.hu/"
+        const val BASE_URL = "http://kasimadalan.pe.hu/"
+        const val YEMEK_IMG_ENDPOINT = "yemekler/resimler/"
+        const val USERNAME = "ebruaeo"
 
         fun getProductApi(): ProductApi {
             return RetrofitClient.getClient(BASE_URL).create(ProductApi::class.java)
         }
+
+        fun constructImgUrl(imgName: String) = BASE_URL + YEMEK_IMG_ENDPOINT + imgName
     }
 }
