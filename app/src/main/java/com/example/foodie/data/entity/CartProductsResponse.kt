2395479a3802a0599e442402bd6328450385a6f8
ebruaeo <ebruaeo@hotmail.com>
2024-03-25 11:20:36 +0300

    cleanup

--- app/src/main/java/com/example/foodie/data/entity/CartProductsResponse.kt
+++ app/src/main/java/com/example/foodie/data/entity/CartProductsResponse.kt
@@ -3,6 +3,6 @@ package com.example.foodie.data.entity
 import com.google.gson.annotations.SerializedName
 
 data class CartProductsResponse(
-    @SerializedName("sepet_yemekler")
-    val products: List<CartProduct>,
-    val success: Int)
+    @SerializedName("sepet_yemekler") val products: List<CartProduct>,
+    val success: Int
+)
